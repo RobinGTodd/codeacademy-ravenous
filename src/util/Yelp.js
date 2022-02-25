bc922ab51d09f896a9028665d2cee50eb0f5b7e1
Robin Todd <rgt10@albion.edu>
2022-02-25 15:29:55 -0600

    first commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,35 @@
+const apiKey = '5LVB8-OLeM0AKAz2WafjYj38A0jJ8bE6TQz5xJ_NRbrY4ZXHULbRXljnoydE7V8OB4PqMwJA7u-emOybL0xXeHRvs8LkVakHWRLWTaKdiMaT_BxHKA9InOB3MmIVYnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {headers: {Authorization: `Bearer ${apiKey}`}}
+    ).then(
+      response => {
+        return response.json();
+      }
+    ).then(
+      jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            }
+          })
+        }
+      }
+    )
+  }
+};
+
+export default Yelp;

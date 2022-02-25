a5d57d664ba08e45f4b7995cf8c3681686e88ea5
Robin Todd <rgt10@albion.edu>
2022-02-25 15:47:29 -0600

    added cors anywhere back into yelp api address

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,8 @@ const apiKey = '5LVB8-OLeM0AKAz2WafjYj38A0jJ8bE6TQz5xJ_NRbrY4ZXHULbRXljnoydE7V8O
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(
-      `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+
+      `https://corsanywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {headers: {Authorization: `Bearer ${apiKey}`}}
     ).then(
       response => {
